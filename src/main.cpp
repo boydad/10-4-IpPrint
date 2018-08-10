f8c13f6d6e4a82c7353bcc26ceb240cb3546b2c0
Denis Boyda <boyda_d@mail.ru>
2018-08-10 15:52:30 +1000

    Print string, tuple is added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <type_traits>
+#include <vector>
+#include <list>
 
-template <class T>
-void printIp(std::ostream& os, const T& ip){
+#include "forEachTuple.h"
+
+template <typename T>
+typename std::enable_if <std::is_integral<T>::value>::type
+printIp(std::ostream& os, const T& ip){
 	size_t num = sizeof(ip);
 	const char* ptr = reinterpret_cast<const char*> (&ip);
 	
@@ -11,8 +17,19 @@ void printIp(std::ostream& os, const T& ip){
 	os << std::endl;	
 }
 
-template <typename... Args, template <typename ...Args> class Conteiner>
-void printIp (std::ostream& os, const Conteiner<Args...>& ip){
+//на мой взгляд лучше использовать перегрузку, на в задании требуется специализация
+template <typename T>
+typename std::enable_if <std::is_same<T, std::string>::value >::type
+printIp(std::ostream& os, const T& ip){
+	os << ip << std::endl;
+}
+
+template <typename T>
+typename std::enable_if <
+		std::is_same <std::vector<typename T::value_type>, T>::value ||
+		std::is_same <std::list<typename T::value_type>, T>::value
+	>::type
+printIp (std::ostream& os, const T& ip){
 	for (auto ip_part = ip.cbegin(); ip_part != ip.cend(); ++ip_part) {
 		if (ip_part != ip.cbegin())
 			os << ".";
@@ -21,12 +38,46 @@ void printIp (std::ostream& os, const Conteiner<Args...>& ip){
 	os << std::endl;	
 }
 
+namespace __tuple{
+	struct callback
+	{
+		callback(std::ostream& os): os(os) {};
+		template<typename T>
+		void operator()(int index, T&& t)
+		{                                
+			if(index != 0)
+				os << '.';
+			os << t;
+		}
+	private:
+		std::ostream& os;
+	};
+}
+
+template <typename T>
+decltype(std::tuple_size<T>::value, void())
+printIp (std::ostream& os, T& ip){
+	__tuple::callback out(os);
+	for_each(ip, out);
+	os << std::endl;
+}
+
 int main(int argc, char** argv)
 {
 	printIp(std::cout, char(-1));
 	printIp(std::cout, short(0));
 	printIp(std::cout, int(2130706433));
 	printIp(std::cout, long(8875824491850138409));
-		
+					
+	std::string str("string");
+	printIp(std::cout, str);
+	
+	std::vector<int> tt;
+	tt.push_back(10);
+	printIp(std::cout, tt);
+	
+	auto tmp = std::make_tuple(1,2,3);
+	printIp(std::cout, tmp);
+	
 	return 0;
 }
