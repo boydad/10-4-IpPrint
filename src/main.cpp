392da58f28590a3b0f0ee2c259285820df04240b
Denis Boyda <boyda_d@mail.ru>
2018-08-17 14:00:56 +1000

    Revert "Empty"
    This reverts commit 74903c13d7c71bd991aafc6119e011ec4833edb3.

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+/// \file
+
+/// \mainpage Доманее задание 03 к уроку 10
+///
+/// \section Задача
+///
+/// - Необходимо написать функцию печати ip адресса побайтно в стандартный поток
+/// вывода std::cout, байты должны разделяться точкой. Примеры использования 
+/// функции приведены в main().
+/// - Научиться использовать doxygen.
+/// 
+/// \see main()
+/// \see printIp()
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include "printIp.h"
+
+/*! \brief Основная функция проекта.
+ *
+ * Содержит примеры использования функции printIp().
+ * 
+ */
+int main(int argc, char** argv)
+{
+	printIp(std::cout, char(-1));
+	printIp(std::cout, short(0));
+	printIp(std::cout, int(2130706433));
+	printIp(std::cout, long(8875824491850138409));
+						
+	std::vector<int> vec;
+	vec.emplace_back(1);
+	vec.emplace_back(2);
+	vec.emplace_back(3);
+	vec.emplace_back(15);
+	printIp(std::cout, vec);
+	
+	std::list<int> lst;
+	for(const auto& e: vec)
+		lst.push_front(e);
+	printIp(std::cout, lst);
+	
+	printIp(std::cout, std::make_tuple("t1", "t2", "t3", "t4"));
+	
+	
+	return 0;
+}
